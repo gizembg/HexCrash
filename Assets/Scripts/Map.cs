fcfcf636760f7b782fc3e1da74b2f3c1042af49c
gizem <gizembg@gmail.com>
2019-04-02 10:31:11 +0300

    - Optional colors and grid size feature has created.

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -10,10 +10,26 @@ public class Map : MonoBehaviour
     float xOffset = 0.879f;
     float yOffset = 0.764f;
     public static List<Hex> hexagonList = new List<Hex>();
+    public int colorCount = 5;
+
+    public Color color1 = Color.red;
+    public Color color2 = Color.yellow;
+    public Color color3 = Color.magenta;
+    public Color color4 = Color.blue;
+    public Color color5 = Color.green;
+
+
     //private List<List<Hex>> hexGrid;
 
     void Start()
     {
+        List<Color> colorList = new List<Color>();
+        colorList.Add(color1);
+        colorList.Add(color2);
+        colorList.Add(color3);
+        colorList.Add(color4);
+        colorList.Add(color5);
+
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
@@ -32,12 +48,13 @@ public class Map : MonoBehaviour
                 hexGO.GetComponent<Hex>().y = y;
                 hexGO.transform.SetParent(this.transform);
                 hexGO.isStatic = true;
-
-                Color color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+                Color color = colorList[Random.Range(1, colorList.Count)];
+                //Color color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
 
                 hexGO.transform.GetChild(0).GetComponent<MeshRenderer>().material.color = color;  //hex
                 hexGO.GetComponent<Hex>().color = color;  //Hex_x_y
                 hexagonList.Add(hexGO.GetComponent<Hex>());
+
             }
         }
     }
